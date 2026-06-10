fbc43493b9105f67749fb3fb129150f6c06c453d
Pat desktop <dmtpatricia@gmail.com>
2026-06-10 18:43:43 +0200

    Strore method in pizza controller

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzasController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzasController.java
@@ -7,10 +7,18 @@ import org.lessons.java.spring_la_mia_pizzeria_crud.repo.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -45,10 +53,24 @@ public class PizzasController {
     
     @GetMapping("/create")
     public String create(Model model) {
-        
+
         model.addAttribute("pizza", new Pizza());
 
         return "create";
     }
+    
+    @PostMapping("/create")
+    public String store(
+        @Valid @ModelAttribute("pizza")Pizza pizza,
+        BindingResult bindingResult,
+        Model model
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "/create";
+        }
+        repository.save(pizza);
+        return "redirect:/pizza";
+    }
+    
 
 }
